00d66a30358ca3d0d12c92ca7a151977bb4f3752
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2022-06-22 09:31:18 +0200

    Remove hardcoded Packagist token from repo

--- tests/integration-tests.sh
+++ tests/integration-tests.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-PACKAGIST_TOKEN="3044793b46ca7af35efa022e25dabcc45ffbf0f312333dc1891997746f50"
-COMPOSER_AUTH="composer config --global --auth http-basic.repo.packagist.com token ${PACKAGIST_TOKEN}"
 COMPOSER_UPDATE="composer update --no-interaction -q"
 COMPOSER_TESTS="composer tests:integration"
 
